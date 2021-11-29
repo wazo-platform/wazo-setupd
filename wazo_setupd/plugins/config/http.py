3911a13cf9966a80c3b0950c131945267e3e4014
Alexandre Fournier <afournier@wazo.io>
2021-11-29 17:09:54 -0500

    config: restrict to master tenant

--- wazo_setupd/plugins/config/http.py
+++ wazo_setupd/plugins/config/http.py
@@ -1,8 +1,9 @@
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo.auth_verifier import required_acl
 
+from wazo_setupd.auth import required_master_tenant
 from wazo_setupd.http import AuthResource
 
 
@@ -10,6 +11,7 @@ class ConfigResource(AuthResource):
     def __init__(self, config):
         self._config = config
 
+    @required_master_tenant()
     @required_acl('setupd.config.read')
     def get(self):
         return dict(self._config), 200
