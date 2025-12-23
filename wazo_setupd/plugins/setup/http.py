f6d5d6fe4cc971bd9bdd52a0048bb1ac04108fe9
Charles Langlois <charles.langlois@wazo.io>
2025-12-23 17:27:31 -0500

    plugins.setup: register openapi spec
    using flask-restful and marshmallow integrations
    and some inline yaml docstring

--- wazo_setupd/plugins/setup/http.py
+++ wazo_setupd/plugins/setup/http.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from flask import request
@@ -13,6 +13,31 @@ class SetupResource(ErrorCatchingResource):
         self.service = service
 
     def post(self):
+        """Setup the Wazo Engine.
+        ---
+        tags:
+          - setup
+        operationId: setupWazoEngine
+        summary: Setup the Wazo Engine
+        description: |
+          Initialize the Wazo Engine with provided configuration.
+          This endpoint configures the engine language, admin password, and optionally
+          registers with a Nestbox instance for enterprise management.
+        requestBody:
+          required: true
+          content:
+            application/json:
+              schema: SetupSchema
+        responses:
+          201:
+            description: The setup has been completed
+          400:
+            $ref: '#/components/responses/InvalidRequest'
+          500:
+            $ref: '#/components/responses/InternalServerError'
+          503:
+            $ref: '#/components/responses/ServiceUnavailable'
+        """
         setup_infos = setup_schema.load(request.json)
 
         self.service.setup(setup_infos)
--- wazo_setupd/plugins/setup/plugin.py
+++ wazo_setupd/plugins/setup/plugin.py
@@ -1,12 +1,24 @@
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from apispec import APISpec
+
 from .http import SetupResource
+from .schemas import SetupSchema
 from .services import SetupService
 
 
 class Plugin:
+    def register_spec(self, spec: APISpec):
+        spec.components.schema('SetupSchema', schema=SetupSchema)
+        spec.path(resource=SetupResource, path='/setup')
+
     def load(self, dependencies):
         api = dependencies['api']
+        spec: APISpec = dependencies['spec']
+
         service = SetupService(dependencies['config'], dependencies['stopper'])
         api.add_resource(SetupResource, '/setup', resource_class_args=[service])
+
+        # Register OpenAPI spec
+        self.register_spec(spec)
