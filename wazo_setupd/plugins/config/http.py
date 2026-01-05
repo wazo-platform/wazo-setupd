4ae925868d9aeeaac50c0db03668d674262bfd95
Charles Langlois <charles.langlois@wazo.io>
2026-01-05 17:05:56 -0500

    plugins.config: register openapi spec

--- wazo_setupd/plugins/config/http.py
+++ wazo_setupd/plugins/config/http.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo.auth_verifier import required_acl
@@ -14,4 +14,30 @@ class ConfigResource(AuthResource):
     @required_master_tenant()
     @required_acl('setupd.config.read')
     def get(self):
+        """
+        ---
+        tags:
+          - config
+        operationId: getConfig
+        summary: Show the current configuration
+        description: |
+          Returns the current configuration of the wazo-setupd service.
+
+          **Required ACL:** `setupd.config.read`
+
+          **Note:** This endpoint is restricted to the master tenant.
+        security:
+          - wazo_auth_token:
+            - setupd.config.read
+        responses:
+          200:
+            description: The configuration of the service
+            content:
+              application/json:
+                schema:
+                  type: object
+                  additionalProperties: true
+          401:
+            $ref: '#/components/responses/InvalidRequest'
+        """
         return dict(self._config), 200
--- wazo_setupd/plugins/config/plugin.py
+++ wazo_setupd/plugins/config/plugin.py
@@ -1,11 +1,20 @@
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+from apispec import APISpec
 
 from .http import ConfigResource
 
 
 class Plugin:
+    def register_spec(self, spec: APISpec):
+        spec.path(resource=ConfigResource, path='/config')
+
     def load(self, dependencies):
         api = dependencies['api']
         config = dependencies['config']
+        spec: APISpec = dependencies['spec']
+
         api.add_resource(ConfigResource, '/config', resource_class_args=[config])
+
+        self.register_spec(spec)
